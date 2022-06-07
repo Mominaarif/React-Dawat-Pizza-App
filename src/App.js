804330842ed8d837bb48f3936b9f161269180134
Momina Arif <66367792+Mominaarif@users.noreply.github.com>
2022-06-07 14:10:11 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Order from './components/Order';
+import Product from './components/Product';
+import Contact from './components/Contact';
+import { Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import About from './pages/About';
+import Menu from './pages/Menu';
+import Blog from './pages/Blog';
+import Products from './pages/Products';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="menu" element={<Menu />} />
+        <Route path="products" element={<Products />} />
+        <Route path="blog" element={<Blog />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="product" element={<Product />} />
+        <Route path="order" element={<Order />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
